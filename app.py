dc4982a6c9d8f14ba341fea640e0d57ee98302e7
Alex Mwendwa <alexandermwendwa3@gmail.com>
2026-03-16 07:30:07 +0300

    First Flask backend API server

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to Alex's Backend API!"})
+
+@app.route("/joke")
+def joke():
+    response = requests.get("https://official-joke-api.appspot.com/random_joke")
+    data = response.json()
+
+    return jsonify({
+        "setup": data["setup"],
+        "punchline": data["punchline"]
+    })
+
+@app.route("/github/<username>")
+def github_profile(username):
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return jsonify({"error": "User not found"}), 404
+
+    data = response.json()
+
+    return jsonify({
+        "username": data["login"],
+        "repos": data["public_repos"],
+        "followers": data["followers"],
+        "following": data["following"]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
